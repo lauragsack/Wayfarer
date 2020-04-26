2a2926f04de77ddfd95d5c14b2ace0b65f3b1d5f
KingSri <srirang97@gmail.com>
2020-04-26 00:37:15 -0700

    Adding test buttons to city page

--- src/components/City.js
+++ src/components/City.js
@@ -1,17 +1,24 @@
 
 //importing
 import React, { Component } from 'react';
-
+import './styles.css';
 
 //Trying to render data from seed file before rendering
 
 class City extends Component{
+  // state = {
+  //   city: this.props.city,
+  //   posts: []
+  // }
+        //similar to<p><Link to={`/posts/${props.post._id}`}>{props.post.title}</Link></p>
   render(){
     return (
       <div>
-      <h1>{this.props.city.name}</h1>
-      <img className="mb-5" src={this.props.city.image}/>
+      <h1>San Francisco</h1>
+      <header></header>
 
+      <button onClick='#'>Edit</button>
+      <button onClick='#'>Delete</button>
 
 
       </div>
@@ -19,5 +26,3 @@ class City extends Component{
   }
 }
 export default City;
-
-
