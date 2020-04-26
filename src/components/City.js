69a7abfed1938ebac186fe0809e1a735b7ff2337
KingSri <srirang97@gmail.com>
2020-04-25 19:31:47 -0700

    working on trying to render city page based on seed data, need to flesh out controller and component page associated with city. Currently not working

--- src/components/City.js
+++ src/components/City.js
@@ -1,12 +1,17 @@
+
+//importing
 import React, { Component } from 'react';
 import './styles.css';
 
+//Trying to render data from seed file before rendering
 
 class City extends Component{
   render(){
     return (
       <div>
       <h1>San Francisco</h1>
+      <img src={this.props.city.image} alt = {this.props.city.name}/>
+      <h2>{this.props.city.name}</h2>
       <header></header>
 
 
