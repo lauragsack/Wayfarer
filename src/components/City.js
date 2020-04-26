a735ca80c0eaba36c33d983c50b5502a04e44d2f
jimtie <jimmychen.xin@gmail.com>
2020-04-25 19:47:03 -0700

    7:46

--- src/components/City.js
+++ src/components/City.js
@@ -1,16 +1,19 @@
+
+//importing
 import React, { Component } from 'react';
 import './styles.css';
-import Posts from './Posts';
 
+//Trying to render data from seed file before rendering
 
 class City extends Component{
   render(){
     return (
       <div>
       <h1>San Francisco</h1>
+      <img src={this.props.city.image} alt = {this.props.city.name}/>
+      <h2>{this.props.city.name}</h2>
       <header></header>
 
-      <Posts/>
 
       </div>
     )
