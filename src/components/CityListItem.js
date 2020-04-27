5a7622c1c8e44fdbc39394104f40270f78112852
Seanny Phoenix <seannyphoenix@gmail.com>
2020-04-26 18:43:48 -0700

    Ready to show posts

--- src/components/CityListItem.js
+++ src/components/CityListItem.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom';
 
-class CityListItem extends Component {
-
-  render(){
-    return(
-      <li className="list-group-item m-3 shadow">
-        <Link
-          to={`/cities/${this.props.city._id}`}
-           className="row"
-           >
-          <div className="col-4">
-            <img
-              className="rounded"
-              src={this.props.city.images.icon.src}
-              alt={this.props.city.name} />
-          </div>
-          <div className="col-8">
-            <h4>{this.props.city.name}</h4>
-          </div>
-        </Link>
-      </li>
-    );
-  }
+function CityListItem(props){
+  return(
+    <li className="list-group-item">
+      <Link
+        to={`/cities/${props.city._id}`}
+        className="row"
+        >
+        <div className="col-4">
+          <img
+            className="rounded"
+            src={props.city.images.icon.src}
+            alt={props.city.name} />
+        </div>
+        <div className="col-8">
+          <h4>{props.city.name}</h4>
+        </div>
+      </Link>
+    </li>
+  );
 }
 
-export default CityListItem
+
+export default CityListItem;
