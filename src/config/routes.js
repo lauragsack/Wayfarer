65d00caf40726749725a9178057e5bc60863bdef
KingSri <srirang97@gmail.com>
2020-04-27 10:13:23 -0700

    removing city.js file

--- src/config/routes.js
+++ src/config/routes.js
@@ -4,7 +4,6 @@ import { Switch, Route } from 'react-router-dom'
 import Home from '../components/Home'
 import ProfileContainer from '../containers/ProfileContainer'
 import CitiesContainer from '../containers/CitiesContainer'
-import City from '../components/City'
 
  function Routes(props){
    return(
